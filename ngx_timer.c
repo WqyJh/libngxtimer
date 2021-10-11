4aa14619553165119f6afba9b0520c1016520065
WqyJh <781345688@qq.com>
2021-10-11 13:20:03 +0000

    Add inline

--- ngx_timer.c
+++ ngx_timer.c
@@ -3,25 +3,25 @@
 #include <rte_cycles.h>
 
 // 初始化
-int ngx_timer_init(ngx_timer_t *timer) {
+ngx_inline int ngx_timer_init(ngx_timer_t *timer) {
     ngx_rbtree_init(&timer->rbtree, &timer->sentinel,
                     ngx_rbtree_insert_timer_value);
     return 0;
 }
 
 // 添加定时任务
-void ngx_timer_add(ngx_timer_t *timer, ngx_timer_entry_t *te, uint64_t expire_time) {
+ngx_inline void ngx_timer_add(ngx_timer_t *timer, ngx_timer_entry_t *te, uint64_t expire_time) {
     te->timer.key = expire_time;
     ngx_rbtree_insert(&timer->rbtree, &te->timer);
 }
 
 // 取消定时
-void ngx_timer_cancel(ngx_timer_t *timer, ngx_timer_entry_t *te) {
+ngx_inline void ngx_timer_cancel(ngx_timer_t *timer, ngx_timer_entry_t *te) {
     ngx_rbtree_delete(&timer->rbtree, &te->timer);
 }
 
 // 依次执行到期任务
-void ngx_timer_tick(ngx_timer_t *timer, uint64_t now, void *ctx) {
+ngx_inline void ngx_timer_tick(ngx_timer_t *timer, uint64_t now, void *ctx) {
     ngx_timer_entry_t *te;
     ngx_rbtree_node_t *node, *root, *sentinel;
     sentinel = timer->rbtree.sentinel;
